457897c48c73f8967a8c05b20576a7d5bf302223
kamranabilov <kamrangab@code.edu.az>
2022-02-15 16:19:42 +0400

    added kami

---
+++ 15.02.22 TodayTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _15._02._22_TodayTask
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string[] arr = { "My New Project and Today HomeWork" };
+        //    Home(ref arr);
+        //}
+
+        //public static void Home(ref string[] arr)
+        //{            
+        //    foreach (string text in arr)
+        //    {
+        //        foreach (char letter in text)
+        //        {
+        //            if (char.IsUpper(letter))
+        //            {
+        //                Console.Write(char.ToLower(letter));
+        //            }
+        //            else
+        //            {
+        //                Console.Write(char.ToUpper(letter));
+        //            }
+        //        }
+        //    }
+
+        //}
+    }
+}
+            
+
+            
